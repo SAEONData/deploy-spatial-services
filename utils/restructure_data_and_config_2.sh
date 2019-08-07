98ff1a331c68d88bea7380096f4600b79654124f
bryan@saeon.ac.za <bryan@saeon.ac.za>
2019-08-07 12:27:17 +0000

    added utils and docs for inst 2 for data config restruc

---
+++ utils/restructure_data_and_config_2.sh
@@ -0,0 +1,17 @@
+INST_2_DIR=/mnt/media2/geoservers_app01/GeoServer/Instance\ 2/data_dir
+
+DEST_DATA_DIR=/mnt/media2/geoservers_app01/restructured/spatial-data/instance_2/
+DEST_CONF_DIR=/mnt/media2/geoservers_app01/restructured/geoserver-config/instance_2/
+
+for i in coverages data GEO uploaded Uploader_test
+do
+  #sudo cp -r $INST_1_DIR/$i $DEST_DATA_DIR  
+  sudo cp -r "$INST_2_DIR/$i" $DEST_DATA_DIR
+done
+
+for i in demo global.xml gwc gwc-gs.xml layergroups logging.xml logs palettes plugIns security styles user_projections validation wcs.xml wfs.xml wms.xml workspaces www
+do
+   sudo cp -r "$INST_2_DIR/$i" $DEST_CONF_DIR
+done
+
+
